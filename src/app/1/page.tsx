c11efcf837fa13ca7b6debac2a8e4652697989a6
Oscar Treviño <otrevinog29@gmail.com>
2025-02-17 12:13:26 -0600

    page 1 & 2

--- src/app/1/page.tsx
+++ src/app/1/page.tsx
@@ -1,5 +1,73 @@
 import React from "react";
 
 export default function Page1() {
-  return <div>page1</div>;
+  return (
+    <div>
+      <div className="w-full">
+        <div className="container mx-auto flex flex-col gap-10 p-5">
+          <div className="flex flex-row gap-10">
+            <div className="flex gap-4 flex-col flex-1">
+              <div className="flex gap-2 flex-col">
+                <h2 className="text-xl md:text-3xl tracking-tighter lg:max-w-xl font-regular text-left">
+                  Lorem ipsum commodo nulla pariatur dolore ex.
+                </h2>
+                <p className="text-lg max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+                  quasi excepturi, debitis quia ipsa voluptatum! Non assumenda
+                  sequi magnam.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-200 rounded-md w-full aspect-video h-full flex-1"></div>
+          </div>
+          <div className="flex flex-row gap-10">
+            <div className="bg-gray-200 rounded-md w-full aspect-video h-full flex-1"></div>
+            <div className="flex gap-4 flex-col flex-1">
+              <div className="flex gap-2 flex-col">
+                <h2 className="text-xl md:text-3xl tracking-tighter lg:max-w-xl font-regular text-left">
+                  Lorem ipsum commodo nulla pariatur dolore ex.
+                </h2>
+                <p className="text-lg max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+                  quasi excepturi, debitis quia ipsa voluptatum! Non assumenda
+                  sequi magnam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row gap-10">
+            <div className="flex gap-4 flex-col flex-1">
+              <div></div>
+              <div className="flex gap-2 flex-col">
+                <h2 className="text-xl md:text-3xl tracking-tighter lg:max-w-xl font-regular text-left">
+                  Lorem ipsum commodo nulla pariatur dolore ex.
+                </h2>
+                <p className="text-lg max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+                  quasi excepturi, debitis quia ipsa voluptatum! Non assumenda
+                  sequi magnam.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-200 rounded-md w-full aspect-video h-full flex-1"></div>
+          </div>
+          <div className="flex flex-row gap-10">
+            <div className="bg-gray-200 rounded-md w-full aspect-video h-full flex-1"></div>
+            <div className="flex gap-4 flex-col flex-1">
+              <div className="flex gap-2 flex-col">
+                <h2 className="text-xl md:text-3xl tracking-tighter lg:max-w-xl font-regular text-left">
+                  Lorem ipsum commodo nulla pariatur dolore ex.
+                </h2>
+                <p className="text-lg max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+                  quasi excepturi, debitis quia ipsa voluptatum! Non assumenda
+                  sequi magnam.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/2/page.tsx
+++ src/app/2/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Page2() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="mb-4 text-3xl font-bold text-gray-900">Not Found</h1>
+      <Link
+        href="/"
+        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-xl text-sm px-5 py-3"
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
 }
